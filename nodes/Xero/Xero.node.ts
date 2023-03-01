2d348be925c1431ce9c0d08cc0f430401b0e7aef
e513855 <jonas.lauber@sbb.ch>
2023-03-01 23:12:04 +0100

    [JLR] Added few methods to get Xero config

--- nodes/Xero/Xero.node.ts
+++ nodes/Xero/Xero.node.ts
@@ -36,14 +36,109 @@ export class Xero implements INodeType {
 				displayName: 'Tenant ID',
 				name: 'tenantId',
 				type: 'options',
-				description:
-					'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
+				noDataExpression: true,
 				typeOptions: {
 					loadOptionsMethod: 'getTenants',
 				},
 				default: '',
 				required: true,
 			},
+			{
+				displayName: 'Set Branding Theme',
+				name: 'setBrandingTheme',
+				type: 'boolean',
+				default: false,
+				noDataExpression: true
+			},
+			{
+				displayName: 'Branding Theme',
+				name: 'brandingTheme',
+				type: 'options',
+				noDataExpression: true,
+				typeOptions: {
+					loadOptionsDependsOn: ['tenantId'],
+					loadOptionsMethod: 'getBrandingThemes',
+				},
+				displayOptions: {
+					show: {
+						setBrandingTheme: [true],
+					},
+				},
+				default: '',
+				required: false,
+			},
+			{
+				displayName: 'Set Tax Rate',
+				name: 'setTaxRate',
+				type: 'boolean',
+				default: false,
+				noDataExpression: true
+			},
+			{
+				displayName: 'Tax Rate',
+				name: 'taxRate',
+				type: 'options',
+				noDataExpression: true,
+				typeOptions: {
+					loadOptionsDependsOn: ['tenantId'],
+					loadOptionsMethod: 'getTaxRates',
+				},
+				displayOptions: {
+					show: {
+						setTaxRate: [true],
+					},
+				},
+				default: '',
+				required: false,
+			},
+			{
+				displayName: 'Set Account Code',
+				name: 'setAccountCode',
+				type: 'boolean',
+				default: false,
+				noDataExpression: true
+			},
+			{
+				displayName: 'Account Code',
+				name: 'accountCode',
+				type: 'options',
+				noDataExpression: true,
+				typeOptions: {
+					loadOptionsDependsOn: ['tenantId'],
+					loadOptionsMethod: 'getAccountCodes',
+				},
+				displayOptions: {
+					show: {
+						setAccountCode: [true],
+					},
+				},
+				default: '',
+				required: false,
+			},
+			{
+				displayName: 'Set Currency',
+				name: 'setCurrency',
+				type: 'boolean',
+				default: false,
+				noDataExpression: true
+			},
+			{
+				displayName: 'Account Code',
+				name: 'currency',
+				type: 'options',
+				noDataExpression: true,
+				typeOptions: {
+					loadOptionsDependsOn: ['tenantId'],
+					loadOptionsMethod: 'getCurrencies',
+				},
+				displayOptions: {
+					show: {
+						setCurrency: [true],
+					},
+				},
+				default: '',
+				required: false,
+			}
 		],
 	};
 
@@ -71,6 +166,68 @@ export class Xero implements INodeType {
 				}
 				return returnData;
 			},
+			async getBrandingThemes(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+				const tenantId = this.getCurrentNodeParameter('tenantId');
+				const returnData: INodePropertyOptions[] = [];
+				const { BrandingThemes: themes } = await xeroApiRequest.call(
+					this,
+					'GET',
+					'/BrandingThemes',
+					{tenantId},
+				);
+				for (const theme of themes) {
+					returnData.push({
+						name: theme.Name,
+						value: theme.BrandingThemeID,
+					});
+				}
+				return returnData;
+			},
+			async getTaxRates(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+				const tenantId = this.getCurrentNodeParameter('tenantId');
+				const returnData: INodePropertyOptions[] = [];
+				const { TaxRates: taxRates } = await xeroApiRequest.call(
+					this,
+					'GET',
+					'/TaxRates',
+					{tenantId},
+				);
+				for (const taxRate of taxRates) {
+					returnData.push({
+						name: `${taxRate.Name} (${taxRate.TaxType})`,
+						value: taxRate.TaxType,
+					});
+				}
+				return returnData;
+			},
+			async getAccountCodes(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+				const tenantId = this.getCurrentNodeParameter('tenantId');
+				const returnData: INodePropertyOptions[] = [];
+				const { Accounts: accounts } = await xeroApiRequest.call(this, 'GET', '/Accounts', {
+					tenantId,
+				});
+				for (const account of accounts) {
+					returnData.push({
+						name: `${account.Name} (${account.Code})`,
+						value: account.Code,
+					});
+				}
+				return returnData;
+			},
+			async getCurrencies(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+				const tenantId = this.getCurrentNodeParameter('tenantId');
+				const returnData: INodePropertyOptions[] = [];
+				const { Currencies: currencies } = await xeroApiRequest.call(this, 'GET', '/Currencies', {
+					tenantId,
+				});
+				for (const currency of currencies) {
+					returnData.push({
+						name: currency.Description,
+						value: currency.Code,
+					});
+				}
+				return returnData;
+			}
 		},
 	};
 
@@ -82,7 +239,11 @@ export class Xero implements INodeType {
 		for (let i = 0; i < length; i++) {
 			try {
 				responseData = {
-					tenantId: this.getNodeParameter('tenantId', i) as string
+					tenantId: this.getNodeParameter('tenantId', i) as string,
+					brandingTheme: this.getNodeParameter('brandingTheme', i) as string,
+					taxRate: this.getNodeParameter('taxRate', i) as string,
+					accountCode: this.getNodeParameter('accountCode', i) as string,
+					currency: this.getNodeParameter('currency', i) as string,
 				};
 
 				const executionData = this.helpers.constructExecutionMetaData(
